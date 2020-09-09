24498c196091b0d8032475ab92a4e539cf8cadaa
marysue <mary@marylark.com>
2020-09-09 14:40:03 -0600

    Finished up/down voting

--- public/events.js
+++ public/events.js
@@ -34,5 +34,40 @@ window.addEventListener("DOMContentLoaded", () => {
             });
     });
 
+    document.getElementById('upvote').addEventListener('click', () => {
+        fetch(`/kitten/upvote`, {
+            method: "PATCH",
+          })
+          .then (res => {
+              if (!res.ok) throw Error(res.statusText);
+              return res.json();
+          })
+          .then (data => {
+              console.log(data);
+              let score = document.getElementsByClassName('score');
+              score[0].innerHTML = data.score;
+          })
+          .catch (err => {
+            alert(res.message);
+          })
+    });
+
+    document.getElementById('downvote').addEventListener('click', () => {
+        fetch(`/kitten/downvote`, {
+            method: "PATCH",
+          })
+          .then (res => {
+              if (!res.ok) throw Error(res.statusText);
+              return res.json();
+          })
+          .then (data => {
+              console.log(data);
+              let score = document.getElementsByClassName('score');
+              score[0].innerHTML = data.score;
+          })
+          .catch (err => {
+             alert(res.message);
+          })
+    });
 
 });
