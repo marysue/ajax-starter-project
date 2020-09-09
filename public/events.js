cbc87029f5edbfb5c9c300406fc9297875393dfb
Michael Jensen <mcjensen24@gmail.com>
2020-09-09 15:33:52 -0400

    finished phase 3

--- public/events.js
+++ public/events.js
@@ -1,7 +1,38 @@
 window.addEventListener("DOMContentLoaded", () => {
 
     fetch("/kitten/image")
-        .then(res =>  { return res.json()})
-        .then( (data) => console.log(data))
+        .then(res => {
+            if (!res.ok) {
+                throw res;
+            } return res.json()
+        })
+        .then((data) => {
+            let imgElt = document.querySelector('img');
+            imgElt.setAttribute("src", data.src);
+        })
+        .catch(error => {
+            error.json().then((res) => alert(res.message));
+        });
+
+    document.getElementById('new-pic').addEventListener('click', () => {
+        let loader = document.querySelector('div');
+        loader.innerHTML = 'loading...';
+
+        fetch("/kitten/image")
+            .then(res => {
+                if (!res.ok) {
+                    throw res;
+                } return res.json()
+            })
+            .then((data) => {
+                loader.innerHTML = '';
+                let imgElt = document.querySelector('img');
+                imgElt.setAttribute("src", data.src);
+            })
+            .catch(error => {
+                error.json().then((res) => alert(res.message));
+            });
+    });
+
 
 });
